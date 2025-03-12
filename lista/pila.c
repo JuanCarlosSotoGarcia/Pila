4b22e1f85836116ecb4fefbce9c6a64b1539bdbb
JuanCarlosSotoGarcia <carloscc1125@gmail.com>
2025-03-12 10:52:07 -0600

    pila.c

--- lista/pila.c
+++ lista/pila.c
@@ -1,53 +1,53 @@
+
 #include "pila.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-Pila *vacia()
-{
-    Pila *p;
-    _________________
-    _________________
+
+Pila *vacia() {
+    Pila *p = (Pila *)malloc(sizeof(Pila));
+    p->cima = NULL;
+    p->len=0;
     return p;
 }
 
-bool es_vacia(Pila *p){
-    return ___________________;
+bool es_vacia(Pila *p) {
+    return p->cima == NULL;
 }
 
-void apilar(Pila *p, Dato d)
-{
+void apilar(Pila *p, Dato d) {
     nodo *nuevo = crear_nodo(d);
-    ___________________________;
-    ___________________________;
-    ___________________________;
-
+    nuevo->siguiente = p->cima;
+    p->cima = nuevo;
 }
 
-void desapilar(Pila *p)
-{
-    if(!es_vacia(p)){
-        ___________________________;
-        ___________________________;
-        ___________________________;
-    }else
+void desapilar(Pila *p) {
+    if(!es_vacia(p)) {
+        nodo *temp = p->cima;
+        p->cima = p->cima->siguiente;
+        p->sig=NULL
+        borrar_nodo(temp);
+    } else 
         printf("La pila está vacía\n");
-}
-
-Dato cima(Pila *p)
-{
-    return ______________________________;
-}
-
-void print_pila(Pila p)
-{
     
-    if(es_vacia(&p)){
-        ____________________________________________________
-        ____________________________________________________
-        ____________________________________________________
+}
+
+Dato cima(Pila *p) {
+    if(!es_vacia(p)) {
+        return p->cima->dato;
     }
+    printf("La pila está vacía\n");
+    return (Dato){0};
+}
+
+void print_pila(Pila p) {
+    if(es_vacia(&p)) {
+        printf("[]\n");
+        return;
+    }
+    
     printf("[ ");
-    ____________________________________________________
-    ____________________________________________________
-    ____________________________________________________
-    printf("]\n");
+    for(nodo *temp=p.cima;temp !=NULL; temp=temp ->sig){
+      printf("%&" 
 }
