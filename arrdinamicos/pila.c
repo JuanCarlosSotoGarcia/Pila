0d4d1799e9a0f9692b285f534a2f7bcd90987162
JuanCarlosSotoGarcia <carloscc1125@gmail.com>
2025-03-12 10:10:41 -0600

    pila.c

--- arrdinamicos/pila.c
+++ arrdinamicos/pila.c
@@ -1,54 +1,72 @@
 #include "pila.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
+typedef struct {
+    Dato *datos; 
+    int cima;    
+    int len;    
+} Pila;
 
-Pila vacia(int tam)
-{
-    
-    ________________________________________
-    ________________________________________
-    ________________________________________
-    ________________________________________
-    ________________________________________    
+Pila vacia(int tam) {
+    Pila p;
+p.len=len;
+    p.datos = (Dato *)malloc(sizeof(Dato)*p.len);
+    p.cima = -1;
+    p.len = tam; 
+    return p;
 }
 
-bool es_vacia(Pila p){
-    return ________________________________________    
+bool es_vacia(Pila p) {
+    return p.cima == -1; 
 }
 
-
-void apilar(Pila *p, Dato d)
-{
-    if(p->cima < p->len-1){
-        ________________________________________
-        ________________________________________
-    }else
+void apilar(Pila *p, Dato d) {
+    if(p->cima < p->len - 1) {
+        p->cima++;
+        p->datos[p->cima] = d;
+        return;
+       }
         printf("La pila está llena\n");
-}
-
-void desapilar(Pila *p)
-{
-    if(p->cima != -1){
-        ______________________________________
-    }else
-        printf("La pila está vacía\n");
-}
-
-Dato cima(Pila p)
-{
-    return _____________________________________
-}
-
-void print_pila(Pila p)
-{
     
-    if(es_vacia(p)){
-        ________________________________________
-        _______________________________________
+}
+
+void desapilar(Pila *p) {
+    if(p->cima != -1) {
+        p->cima--; 
+    } else{
+        printf("La pila está vacía\n");
     }
+}
+
+Dato cima(Pila p) {
+    if(!es_vacia(p)) {
+        return p.datos[p.cima]; 
+    }
+    
+    printf("La pila está vacía\n");
+    return (Dato){0};
+}
+
+void print_pila(Pila p) {
+    if(es_vacia(p)) {
+        printf("La pila está vacía\n");
+        return;
+    }
+    
     printf("[ ");
-    ________________________________________
-    ________________________________________
-    ________________________________________
+    for(int i = 0; i <= p.cima; i++) {
+        print_dato(p.datos[i]);
+        if(i < p.cima) {
+            printf(", "); 
+        }
+    }
     printf("]\n");
+}
+
+void liberar_pila(Pila *p) {
+    free(p->datos); 
+    p->cima = -1;   
+    p->len = 0;     
 }
